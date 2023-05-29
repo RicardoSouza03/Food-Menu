7a0464d49e07c751df951e472ab1f24166a9ecd0
Ricardo Souza <rl03souza@gmail.com>
2023-05-29 20:10:05 -0300

    Feat: create routes on main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import ByCatergory from './Pages/Category.tsx';
+import AllCategories from './Pages/AllCategories.tsx';
+import MealPage from './Pages/MealPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AllCategories />,
+  },
+  {
+    path: "/category/:categoryName",
+    element: <ByCatergory />,
+  },
+  {
+    path: "/meal/:mealId",
+    element: <MealPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

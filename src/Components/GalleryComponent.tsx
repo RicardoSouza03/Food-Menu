ae4d05e14a9f60f61e9ac698ab456dd75817d8bd
Ricardo Souza <rl03souza@gmail.com>
2023-05-29 20:06:40 -0300

    Feat: create gallery component

---
+++ src/Components/GalleryComponent.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid'
+import CardComponent from "./CardComponent";
+
+type Props = {
+    fetch: Function,
+    isCategories: boolean,
+}
+
+export default function GalleryComponent(props: Props) {
+    const [items, setItems] = useState([])
+
+    const getItems = async () => {
+        const items = await props.fetch()
+        setItems(items)
+    }
+
+    useEffect(() => {
+        getItems()
+    }, [])
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={{ md: 1.5 }} columns={{ xs: 4, sm: 5, md: 16 }}>
+                {items.map((item) => (
+                    <Grid item xs={2} sm={4} md={4} key={item['idCategory'] || item['idMeal']}>
+                            <CardComponent
+                                id={item['idCategory'] || item['idMeal']}
+                                description={item['strCategoryDescription'] || null}
+                                name={item['strCategory'] || item['strMeal']}
+                                thumbImg={item['strCategoryThumb'] || item['strMealThumb']}
+                                isCategories={props.isCategories}
+                            />
+                    </Grid>
+                ))}
+            </Grid>
+        </Box>
+    )
+    
+}
